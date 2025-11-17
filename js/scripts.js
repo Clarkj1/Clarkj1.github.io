e7f83c6ec0cf514ad84615948f6dcce4818c10cd
Clarkj1 <clarkkjoseph1@gmail.com>
2025-11-17 18:32:29 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,15 @@
 // Scripts
 // 
 
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
@@ -18,15 +27,25 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     };
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
+    // --- THE FIX IS BELOW ---
+
+    // Get all the links in the navigation menu
+    const navLinks = document.querySelectorAll('#navbarResponsive .nav-link');
+    
+    // Get the actual menu element that collapses
+    const menuCollapsible = document.getElementById('navbarResponsive');
+
+    // Loop through every link
+    navLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+            // Check if the hamburger button is visible (meaning we are on mobile)
+            const navbarToggler = document.body.querySelector('.navbar-toggler');
+            
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
+                // Use Bootstrap's official API to close the menu cleanly
+                // We get the existing instance or create a new one if it doesn't exist
+                const bsCollapse = bootstrap.Collapse.getInstance(menuCollapsible) || new bootstrap.Collapse(menuCollapsible, { toggle: false });
+                bsCollapse.hide();
             }
         });
     });
